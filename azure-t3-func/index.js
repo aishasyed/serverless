c375bf33acb73f59de4c72285956e66c6ff6924c
Aisha Syed <aisha.syed@utah.edu>
2017-12-21 06:06:36 -0700

    Update

--- azure-t3-func/index.js
+++ azure-t3-func/index.js
@@ -20,7 +20,7 @@ module.exports = function (context, req) {
 
 	context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "Hello"
+        body: "Hellow"
     };
 
     context.done();
